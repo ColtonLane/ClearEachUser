42d2eea36cfd40ea3dba2911fe9aa8736bc7904f
ColtonLane <coltonlane314@gmail.com>
2024-11-22 13:56:52 -0500

    mmmm

--- ClearEachUser.cpp
+++ ClearEachUser.cpp
@@ -69,7 +69,7 @@ void progressBar(const int initialTotal) {
         int barWidth = 70;
 
         //The following code is printed to the bottom of the terminal and updated each second to provide a timer and constant progress bar
-        std::cout << "(" << int(timeElapsed)/60 << ":" << int(timeElapsed) % 6 << int(timeElapsed) % 60 << ") "; //This prints the time in the format "m:s"
+        std::cout << "(" << int(timeElapsed)/60 << ":" << (int(timeElapsed) % 6) / 10 << (int(timeElapsed) % 60) / 10 << ") "; //This prints the time in the format "m:s"
         std::cout << deletedCount << " out of " << initialTotal << " deleted. ";
         std::cout << "["; 
         int pos = barWidth * progress;
@@ -140,7 +140,8 @@ int mainLoop() {
         directoryPath = defaultUserPath; 
     }
 
-    initialUserSpaceMB = getFolderSize(directoryPath)/bytesToMB; // Variable to store total space used by the users' AppData folders before starting deletion
+    fs::path directoryLiteralPath (directoryPath);
+    initialUserSpaceMB = getFolderSize(directoryLiteralPath)/bytesToMB; // Variable to store total space used by the users' AppData folders before starting deletion
 
     try {
         // Iterate over the user directories
